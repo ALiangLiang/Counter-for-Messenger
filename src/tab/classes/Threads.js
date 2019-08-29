c0561bef79c8b6f616bfe98c2f1050a46cdbf3ac
ALiangLiang <me@aliangliang.top>
2019-08-29 23:47:25 +0800

    fix

--- src/tab/classes/Threads.js
+++ src/tab/classes/Threads.js
@@ -4,7 +4,6 @@ export default class Threads extends Array {
    * @param {Array<Object>} threadsData threads with Array.
    */
   constructor (threadsData) {
-    console.trace(threadsData)
     super(...threadsData)
   }
 
